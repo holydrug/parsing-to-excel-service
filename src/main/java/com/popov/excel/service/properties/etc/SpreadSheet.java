a913fb3f44bb9db4244a23207911cb58e63cb038
holydrug <nonprofitxzpro@gmail.com>
2022-09-08 13:37:07 +0300

    added base Application class

---
+++ src/main/java/com/popov/excel/service/properties/etc/SpreadSheet.java
@@ -0,0 +1,15 @@
+package com.popov.excel.service.properties.etc;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class SpreadSheet {
+    private String urlOfNBA;
+    private String urlOfSIAP;
+    private String urlOfCHALLENGER;
+    private String urlOfSPE;
+}
