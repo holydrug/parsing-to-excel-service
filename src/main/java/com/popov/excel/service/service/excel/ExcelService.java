d126de6da6de366a5722668e3d4b8fa3efc93d86
holydrug <nonprofitxzpro@gmail.com>
2022-09-09 17:43:02 +0300

    Drop files from .gitignore

--- src/main/java/com/popov/excel/service/service/excel/ExcelService.java
+++
@@ -1,5 +0,0 @@
-package com.popov.excel.service.service.excel;
-
-public interface ExcelService {
-    void init();
-}
--- src/main/java/com/popov/excel/service/service/mail/JavaMailSenderService.java
+++
@@ -1,6 +0,0 @@
-package com.popov.excel.service.service.mail;
-
-
-public interface JavaMailSenderService {
-    void send();
-}
