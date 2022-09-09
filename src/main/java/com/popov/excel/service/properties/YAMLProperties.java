39146637eac0fce079159f39e89ff31d9fe61220
holydrug <nonprofitxzpro@gmail.com>
2022-09-09 16:51:04 +0300

    updated properties

--- src/main/java/com/popov/excel/service/properties/YAMLProperties.java
+++ src/main/java/com/popov/excel/service/properties/YAMLProperties.java
@@ -1,5 +1,6 @@
 package com.popov.excel.service.properties;
 
+import com.popov.excel.service.properties.etc.MailStructure;
 import com.popov.excel.service.properties.etc.SpreadSheet;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -7,7 +8,6 @@ import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.context.annotation.ApplicationScope;
 
 @Getter
 @Setter
