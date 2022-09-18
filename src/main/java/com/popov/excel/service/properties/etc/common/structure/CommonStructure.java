ed5e1c76f08ea3298ddb5eb85e0ca188ac203fa9
holydrug <nonprofitxzpro@gmail.com>
2022-09-18 16:29:03 +0300

    added CommonStructure entity to receive all data from frontend and set in to proper fields in another classes

---
+++ src/main/java/com/popov/excel/service/properties/etc/common/structure/CommonStructure.java
@@ -0,0 +1,16 @@
+package com.popov.excel.service.properties.etc.common.structure;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CommonStructure {
+    private String username;
+    private String password;
+    private String receiver;
+    private String urlOfNBA;
+    private String urlOfSIAP;
+    private String urlOfCHALLENGER;
+    private String urlOfSPE;
+}
